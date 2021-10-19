de2abd9f9b8ef039894ea1896d4d5c67fc06fc86
Yiweiwangau <yiweiwangau@gmail.com>
2021-10-19 22:21:29 +1100

    Added Searchbar and markers
    Added Search bar
    Markers will be created for searched locations

--- app/src/main/java/comp5216/sydney/edu/au/findmygym/ui/map/MapFragment.java
+++ app/src/main/java/comp5216/sydney/edu/au/findmygym/ui/map/MapFragment.java
@@ -1,5 +1,7 @@
 package comp5216.sydney.edu.au.findmygym.ui.map;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -18,6 +21,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 import comp5216.sydney.edu.au.findmygym.R;
 import comp5216.sydney.edu.au.findmygym.databinding.FragmentMapBinding;
@@ -30,15 +36,17 @@ public class MapFragment extends Fragment implements OnMapReadyCallback
 	private MapViewModel mapViewModel;
 	private FragmentMapBinding binding;
 	private SupportMapFragment mMapFragment;
+	private SearchView mSearchView;
+
 	public View onCreateView(@NonNull LayoutInflater inflater,
 	                         ViewGroup container, Bundle savedInstanceState)
 	{
 		mapViewModel =
 				new ViewModelProvider(this).get(MapViewModel.class);
-		
+
 		binding = FragmentMapBinding.inflate(inflater, container, false);
 		View root = binding.getRoot();
-		
+
 		final TextView textView = binding.textMap;
 		mapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>()
 		{
@@ -50,34 +58,80 @@ public class MapFragment extends Fragment implements OnMapReadyCallback
 		});
 		return root;
 	}
-	
+
 	@Override
 	public void onDestroyView()
 	{
 		super.onDestroyView();
 		binding = null;
 	}
-	
+
 	@Override
 	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
 	{
 		super.onViewCreated(view, savedInstanceState);
-		
+
 		mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_mapView);
+
+		SearchView mSearchView = getView().findViewById(R.id.idSearchView);
+		mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+			@Override
+			public boolean onQueryTextSubmit(String query) {
+				// on below line we are getting the
+				// location name from search view.
+				String location = mSearchView.getQuery().toString();
+
+				// below line is to create a list of address
+				// where we will store the list of all address.
+				List<Address> addressList = null;
+
+				// checking if the entered location is null or not.
+				if (location != null || location.equals("")) {
+					// on below line we are creating and initializing a geo coder.
+					Geocoder geocoder = new Geocoder(getActivity());
+					try {
+						// on below line we are getting location from the
+						// location name and adding that location to address list.
+						addressList = geocoder.getFromLocationName(location, 1);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					// on below line we are getting the location
+					// from our list a first position.
+					Address address = addressList.get(0);
+
+					// on below line we are creating a variable for our location
+					// where we will add our locations latitude and longitude.
+					LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+					// on below line we are adding marker to that position.
+					mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+
+					// below line is to animate camera to that position.
+					mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+				}
+				return false;
+			}
+
+			@Override
+			public boolean onQueryTextChange(String newText) {
+				return false;
+			}
+		});
 		mMapFragment.getMapAsync(this);
 	}
-	
+
 	@Override
 	public void onMapReady(GoogleMap googleMap)
 	{
 		mMap = googleMap;
-		
+
 		// Add a marker in Sydney and move the camera
 		LatLng sydney = new LatLng(-34, 151);
 		mMap.addMarker(new MarkerOptions()
 				.position(sydney)
 				.title("Marker in Sydney"));
 		mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-	
+
 	}
 }
