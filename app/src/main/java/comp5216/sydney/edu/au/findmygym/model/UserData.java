066c1e9536c03ed7f5819bc13f9c52859cb0c13b
Yiweiwangau <yiweiwangau@gmail.com>
2021-10-25 22:21:17 +1100

    Update UserData.java

--- app/src/main/java/comp5216/sydney/edu/au/findmygym/model/UserData.java
+++ app/src/main/java/comp5216/sydney/edu/au/findmygym/model/UserData.java
@@ -24,7 +24,7 @@ import comp5216.sydney.edu.au.findmygym.R;
 public class UserData extends LiveData<UserData>
 {
 	private final String TAG = "[UserData]";
-	
+
 	private ArrayList<PurchaseRecord> purchaseRecords;
 	private ArrayList<CreditCard> creditCards;
 	private ArrayList<Membership> memberships;
@@ -40,9 +40,10 @@ public class UserData extends LiveData<UserData>
 
 	// todo: mock的list
 	public List<Gym> allGyms;
-	
+	public List<PersonalTrainer> allTrainers;
+
 	private volatile static UserData UserData;
-	
+
 	/**
 	 * Default Constructor
 	 */
@@ -76,132 +77,112 @@ public class UserData extends LiveData<UserData>
 		Gym gym0 = new Gym(
 				0,
 				"Minus Fitness Gym Chatswood",
-				null,
-				null,
-				null,
+				CalendarUtil.stringToCalendarNoDate("08:30"),
+				CalendarUtil.stringToCalendarNoDate("17:30"),
+				19.9,
 				"763 Pacific Hwy, Chatswood NSW 2067",
 				"123-4567",
 				151.1792,
-				-33.79911,
-				null,
-				null
+				-33.79911
 		);
 		Gym gym1 = new Gym(
 				1,
 				"Minus Fitness Crows Nest",
-				null,
-				null,
-				null,
+				CalendarUtil.stringToCalendarNoDate("08:30"),
+				CalendarUtil.stringToCalendarNoDate("17:30"),
+				20,
 				"400 Pacific Hwy, Crows Nest NSW 2065",
 				"123-4567",
 				151.19854,
-				-33.82581,
-				null,
-				null
+				-33.82581
 		);
 		Gym gym2 = new Gym(
 				2,
 				"Fitness Second St Leonards",
-				null,
-				null,
-				null,
+				CalendarUtil.stringToCalendarNoDate("09:00"),
+				CalendarUtil.stringToCalendarNoDate("19:00"),
+				20,
 				"55 Christie St, St Leonards NSW 2065",
 				"123-4567",
 				151.19584,
-				-33.82445,
-				null,
-				null
+				-33.82445
 		);
 		Gym gym3 = new Gym(
 				3,
 				"Fitness Second North Sydney",
-				null,
-				null,
-				null,
+				CalendarUtil.stringToCalendarNoDate("09:00"),
+				CalendarUtil.stringToCalendarNoDate("19:00"),
+				20,
 				"1 Elizabeth Plaza, North Sydney NSW 2060",
 				"123-4567",
-				151.20809,
 				-33.83945,
-				null,
-				null
+				151.20809
 		);
 		Gym gym4 = new Gym(
 				4,
 				"Fitness Second Bond St",
-				null,
-				null,
-				null,
+				CalendarUtil.stringToCalendarNoDate("09:00"),
+				CalendarUtil.stringToCalendarNoDate("19:00"),
+				20,
 				"20 Bond St, Sydney NSW 2000",
 				"123-4567",
-				151.20829,
 				-33.86441,
-				null,
-				null
+				151.20829
 		);
 		Gym gym5 = new Gym(
 				5,
 				"Minus Fitness Market Street",
-				null,
-				null,
-				null,
+				CalendarUtil.stringToCalendarNoDate("09:00"),
+				CalendarUtil.stringToCalendarNoDate("19:00"),
+				16,
 				"25 Market St, Sydney NSW 2000",
 				"123-4567",
-				151.20522,
 				-33.87115,
-				null,
-				null
+				151.20522
 		);
 		Gym gym6 = new Gym(
 				6,
 				"Minus Fitness Waterloo",
-				null,
-				null,
-				null,
+				CalendarUtil.stringToCalendarNoDate("09:00"),
+				CalendarUtil.stringToCalendarNoDate("19:00"),
+				15,
 				"11A Lachlan St, Waterloo NSW 2017",
 				"123-4567",
-				151.21178,
 				-33.90103,
-				null,
-				null
+				151.21178
 		);
 		Gym gym7 = new Gym(
 				7,
-				"Notime Fitness North Sydney",
-				null,
-				null,
-				null,
+				"Notime Fitness North Sydey",
+				CalendarUtil.stringToCalendarNoDate("09:00"),
+				CalendarUtil.stringToCalendarNoDate("19:00"),
+				20,
 				"118 Walker St, North Sydney NSW 2060",
 				"123-4567",
-				151.208801,
 				-33.837711,
-				null,
-				null
+				151.208801
 		);
 		Gym gym8 = new Gym(
 				8,
 				"Notime Fitness City",
-				null,
-				null,
-				null,
+				CalendarUtil.stringToCalendarNoDate("09:00"),
+				CalendarUtil.stringToCalendarNoDate("19:00"),
+				20,
 				"227 Elizabeth St, Sydney NSW 2000",
 				"123-4567",
-				151.2102227,
 				-33.8706586,
-				null,
-				null
+				151.2102227
 		);
 		Gym gym9 = new Gym(
 				9,
 				"Sliver's Gym",
-				null,
-				null,
-				null,
+				CalendarUtil.stringToCalendarNoDate("09:00"),
+				CalendarUtil.stringToCalendarNoDate("19:00"),
+				20,
 				"7-9 West St, North Sydney NSW 2060",
 				"123-4567",
-				151.2052855,
 				-33.8334692,
-				null,
-				null
+				151.2052855
 		);
 		allGyms.add(gym0);
 		allGyms.add(gym1);
@@ -213,21 +194,32 @@ public class UserData extends LiveData<UserData>
 		allGyms.add(gym7);
 		allGyms.add(gym8);
 		allGyms.add(gym9);
+
+		allTrainers = new ArrayList<>();
+		allTrainers.add(new PersonalTrainer(1, "Mary", 30.0));
+		allTrainers.add(new PersonalTrainer(2, "Jack", 35.0));
 	}
 
 	public void addMockReservations()
 	{
 		reservations = new ArrayList<Reservation>();
+		// todo: user id
 		Reservation rev1 = new Reservation(
-				new PersonalTrainer(1, "Marry", null),
+				1,
+				1,
+				1,
 				new Timeslot(CalendarUtil.stringToCalendar("2021-10-23 09:00"), 60)
 		);
 		Reservation rev2 = new Reservation(
-				new PersonalTrainer(0, "Jack", null),
+				1,
+				2,
+				0,
 				new Timeslot(CalendarUtil.stringToCalendar("2021-10-20 09:00"), 60)
 		);
 		Reservation rev3 = new Reservation(
-				new PersonalTrainer(0, "Jack", null),
+				1,
+				2,
+				0,
 				new Timeslot(CalendarUtil.stringToCalendar("2021-10-23 08:00"), 120)
 		);
 
@@ -236,84 +228,116 @@ public class UserData extends LiveData<UserData>
 		reservations.add(rev3);
 	}
 
+	public List<Gym> getAllGyms() {
+		return allGyms;
+	}
+
+	public int getUserId() {
+		// TODO:
+		return 0;
+	}
+
+	public List<PersonalTrainer> getAllTrainers() {
+		return allTrainers;
+	}
+
+	public Gym findGymById(int gymId) {
+		// TODO: firebase
+		for (Gym gym : getAllGyms()) {
+			if (gym.getGymId() == gymId) {
+				return gym;
+			}
+		}
+		return null;
+	}
+
+	public PersonalTrainer findTrainerById(int trainerId) {
+		for (PersonalTrainer trainer : getAllTrainers()) {
+			if (trainer.getTrainerId() == trainerId) {
+				return trainer;
+			}
+		}
+		return null;
+	}
+
 	/**
 	 * PurchaseRecords
 	 */
-	
+
 	public ArrayList<PurchaseRecord> getPurchaseRecords()
 	{
 		return purchaseRecords;
 	}
-	
+
 	public void addPurchaseRecord(PurchaseRecord purchaseRecord)
 	{
 		this.purchaseRecords.add(purchaseRecord);
 		sortPurchaseRecords();
 		postValue(this);
 	}
-	
+
 	public void setPurchaseRecords(ArrayList<PurchaseRecord> purchaseRecords)
 	{
 		this.purchaseRecords = purchaseRecords;
 		sortPurchaseRecords();
 		postValue(this);
 	}
-	
+
 	public void removePurchaseRecord(int position)
 	{
 		this.purchaseRecords.remove(position);
 		postValue(this);
 	}
-	
+
 	/**
 	 * CreditCards
 	 */
-	
+
 	public ArrayList<CreditCard> getCreditCards()
 	{
 		return creditCards;
 	}
-	
+
 	public void addCreditCard(CreditCard creditCard)
 	{
 		this.creditCards.add(0,creditCard);
 		postValue(this);
 	}
-	
+
 	public void setCreditCards(ArrayList<CreditCard> creditCards)
 	{
 		this.creditCards = creditCards;
 		postValue(this);
 	}
-	
+
 	public void removeCreditCard(int position)
 	{
 		this.creditCards.remove(position);
 		Log.e(TAG, "removeCreditCard: "+this.getCreditCards() );
 		postValue(this);
 	}
-	
+
 	/**
 	 * Memberships
 	 */
-	
+
 	public ArrayList<Membership> getMemberships()
 	{
 		return memberships;
 	}
-	
+
 	public void addMembership(Membership membership)
 	{
 		this.memberships.add(0,membership);
 		postValue(this);
 	}
-	
+
 	public void setMemberships(ArrayList<Membership> memberships)
 	{
 		this.memberships = memberships;
 		postValue(this);
 	}
-	
+
 	public void removeMembership(int position)
 	{
 		this.memberships.remove(position);
@@ -324,13 +348,13 @@ public class UserData extends LiveData<UserData>
 	{
 		return firebaseUser;
 	}
-	
+
 	public void setFirebaseUser(FirebaseUser firebaseUser)
 	{
 		this.firebaseUser = firebaseUser;
 		postValue(this);
 	}
-	
+
 	public String getUserName()
 	{
 		if (userName != null)
@@ -342,13 +366,13 @@ public class UserData extends LiveData<UserData>
 		}
 		return "GUEST: JOHN DOE";
 	}
-	
+
 	public void setUserName(String userName)
 	{
 		this.userName = userName;
 		postValue(this);
 	}
-	
+
 	public String getUserMail()
 	{
 		if (userMail != null)
@@ -360,13 +384,13 @@ public class UserData extends LiveData<UserData>
 		}
 		return "Go sign in, NOW!";
 	}
-	
+
 	public void setUserMail(String userMail)
 	{
 		this.userMail = userMail;
 		postValue(this);
 	}
-	
+
 	public Bitmap getUserAvatar()
 	{
 		if (userAvatar != null)
@@ -398,7 +422,7 @@ public class UserData extends LiveData<UserData>
 	// 	}
 	// 	return avatar;
 	// }
-	
+
 	public Uri getUserAvatarUri()
 	{
 		return this.firebaseUser.getPhotoUrl();
@@ -434,7 +458,7 @@ public class UserData extends LiveData<UserData>
 		this.favouriteGyms = favouriteGyms;
 		postValue(this);
 	}
-	
+
 	public void setContext(Context mContext)
 	{
 		this.mContext = mContext;
@@ -478,14 +502,14 @@ public class UserData extends LiveData<UserData>
 		// 具有活跃的观察者时调用
 		Log.d(TAG, "Get an observer!");
 	}
-	
+
 	@Override
 	protected void onInactive()
 	{
 		// 没有任何活跃的观察者时调用
 		Log.d(TAG, "Get no observer!");
 	}
-	
+
 	public void logout()
 	{
 		firebaseUser = null;
@@ -496,7 +520,7 @@ public class UserData extends LiveData<UserData>
 		userStorageRef = null;
 		mContext = null;
 	}
-	
+
 	public void sortPurchaseRecords(){
 		Log.e(TAG,"---------------------"+this.getPurchaseRecords().size());
 		this.purchaseRecords.sort(new Comparator<PurchaseRecord>()
